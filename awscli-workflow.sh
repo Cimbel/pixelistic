8fcb609ab81787fe1d52c36a0536e04b9111794a
Roman Krekhovets <55352072+Cimbel@users.noreply.github.com>
2021-07-28 17:41:04 +0300

    Update awscli-workflow.sh

--- awscli-workflow.sh
+++ awscli-workflow.sh
@@ -3,59 +3,54 @@
 
 # install dependencies
 
-apt update -y && apt upgrade -y
-apt install -y awscli awsebcli unzip zip
-
-pip3 install awscli --user --upgrade
-pip3 install awsebcli --user --upgrade
+pip3 install awscli --user --upgrade 2> /dev/null
+pip3 install awsebcli --user --upgrade 2> /dev/null
 
 
 # create artifact
 
-# cd ./pixelapp_be
-# zip -r $ARTIFACT_NAME ./* ./**/*
-# cd ../ && mkdir my_project_deploy
-# cd my_project_deploy && mv ../pixelapp_be/$ARTIFACT_NAME ./
+cd ./pixelapp_be
+zip -r $ARTIFACT_NAME ./* ./**/*
+cd ../ && mkdir my_project_deploy
+cd my_project_deploy && mv ../pixelapp_be/$ARTIFACT_NAME ./
 
 
 # configure awscli
 
-# mkdir ~/.aws 2> /dev/null
-# cat <<EOF > ~/.aws/config
-# [default]
-# region = $AWS_APP_REGION
-# output = json
-# EOF
+mkdir ~/.aws 2> /dev/null
+cat <<EOF > ~/.aws/config
+[default]
+region = $AWS_APP_REGION
+output = json
+EOF
 
-# cat <<EOF > ~/.aws/credentials
-# [default]
-# aws_access_key_id = $AWS_ACCESS_KEY
-# aws_secret_access_key = $AWS_SECRET_KEY
-# EOF
+cat <<EOF > ~/.aws/credentials
+[default]
+aws_access_key_id = $AWS_ACCESS_KEY
+aws_secret_access_key = $AWS_SECRET_KEY
+EOF
 
 
 # configure config.yml for awsebcli app
 
-# mkdir .elasticbeanstalk
-# cat <<EOF > .elasticbeanstalk/config.yml
-# branch-defaults:
-#   default:
-#     environment: $ENV_NAME
-# deploy:
-#   artifact: $ARTIFACT_NAME
-# global:
-#   application_name: $APP_NAME
-#   default_region: $AWS_APP_REGION
-# EOF
+mkdir .elasticbeanstalk
+cat <<EOF > .elasticbeanstalk/config.yml
+branch-defaults:
+  default:
+    environment: $ENV_NAME
+deploy:
+  artifact: $ARTIFACT_NAME
+global:
+  application_name: $APP_NAME
+  default_region: $AWS_APP_REGION
+EOF
 
 
 # checking if installed
 
 find / -name "eb" -user root -perm 0755 2> /dev/null
 find / -name "aws" -user root -perm 0755 2> /dev/null
-which aws
-which eb
 
 # deploy new version
 
-# $(which eb) deploy --label "Version-2"
+$(which eb) deploy --label "Version-2"
